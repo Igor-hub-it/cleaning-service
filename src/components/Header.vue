8232778ba10fcba2943912dfb2ad7a6f3d76aab2
wonitanje <prod.juswee@gmail.com>
2022-02-13 22:25:21 +1000

    сделал цветовую схему, декор и частичный адаптив

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,24 +5,44 @@
         <img class="header__logo-img" src="/images/Logo.png" alt="Логотип">
         <p>PinClean</p>
       </div>
-      <div class="header__categories categories">
-        <div class="categories__dayly">Ежедневная</div>
-        <div class="categories__after-repair">После ремонта</div>
-        <div class="categories__window-washing">Мытье окон</div>
+      <div class="header__categories">
+        <div class="categ-dayly category">Ежедневная</div>
+        <div class="categ-after-repair category">После ремонта</div>
+        <div class="categ-window-washing category">Мытье окон</div>
       </div>
       <div class="header__contacts">
-        <p>phone</p>
-        <p>ofice</p>
+        <p>+7 (900) 600-90-90</p>
+        <p>г. Владивосток п. Аякс 10</p>
+      </div>
+      <div class="header__burger" @click="openBurgerWindow">
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
     </div>
   </header>
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    openBurgerWindow () {
+
+    }
+  }
+}
+</script>
+
 <style lang="scss" scoped>
   .header {
     width: 100%;
     height: auto;
-    background: #f8f8f8;
+    background: #ccffcc;
 
     &__content {
       width: 80%;
@@ -46,25 +66,78 @@
     &__categories {
       display: flex;
       align-items: center;
+      @media screen and (max-width: 1050px) {
+        display: none;
+      }
     }
 
     &__contacts {
       display: flex;
       gap: 30px;
+      @media screen and (max-width: 1050px) {
+        display: none;
+      }
+    }
+
+    &__burger {
+      width: 30px;
+      height: 25px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      @media screen and (min-width: 1050px) {
+        display: none;
+      }
+      span {
+        height: 4px;
+        width: 100%;
+        background: green;
+      }
     }
   }
 
-  .categories {
-    &__dayly, &__after-repair, &__window-washing {
-      height: 70px;
-      padding: 0 15px;
-      display: flex;
-      align-items: center;
+  .category {
+    width: auto;
+    height: 70px;
+    position: relative;
+    padding: 0 15px;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    &:hover {
+      background: #aaffaa;
     }
-    &__dayly:hover, &__after-repair:hover, &__window-washing:hover {
+    &:hover::before {
+      transform: scaleX(1);
       transition: .5s;
-      background: #e9e9e9;
-      cursor: pointer;
+      // transform-origin: right;
+    }
+    &::before {
+      content: '';
+      width: 100%;
+      height: 4px;
+      position: absolute;
+      background: green;
+      bottom: 0;
+      left: 0;
+      transform: scaleX(0);
+      transform-origin: left;
     }
   }
+  // .category:hover::after {
+  //   width: 100%;
+
+  //   &:hover {
+  //     content: '';
+  //     display: block;
+  //     position: absolute;
+  //     bottom: 0;
+  //     left: 50%;
+  //     transform: translateX(-50%);
+  //     height: 2px;
+  //     width: 0;
+  //     background-color: rgb(0, 0, 250);
+  //     transition: 0.2s ease;
+  //   }
+  // }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,44 @@
 <template>
   <div class="home">
-    <div class="slider">
-      <img 
-        class="slider__slds"
-        v-for="slide in slides"
-        :key="slide.id"
-        v-show="slide.isActive"
-        :src="`/images/${slide.img}`"
-      >
-      <div class="slider__sliders-btns">
-        <button @click="prevSlide" class="slider__prev-slide-btn">&#60;</button>
-        <button @click="nextSlide" class="slider__next-slide-btn">&#62;</button>
+    <div class="slider-and-main-discriptor">
+      <div class="slider">
+        <img 
+          class="slider__slds"
+          v-for="slide in slides"
+          :key="slide.id"
+          v-show="slide.isActive"
+          :src="`/images/${slide.img}`"
+        >
+        <div class="slider__sliders-btns">
+          <button @click="prevSlide" class="slider__prev-slide-btn">&#60;</button>
+          <button @click="nextSlide" class="slider__next-slide-btn">&#62;</button>
+        </div>
+      </div>
+      <div class="main-discriptor">
+        <h1>Клининговая компания PinClean</h1>
+        <hr>
+        <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores numquam, repellat quisquam ratione adipisci nihil excepturi et eum nesciunt odio. Deleniti fugit sapiente sint optio soluta! Dolor quasi voluptas ex itaque, sit ipsa ratione enim tempore aspernatur aliquid quod fuga.</h4>
       </div>
     </div>
     <div class="about-us">
-      <h1>Мы - профессиональная клининговая компания</h1>
-      <h4>За нашими плечами множество коммерчских заказов, а клиенты довольны качеством оказываемых услуг.
-        Мы работаем как с юридическими так и с физическими лицами.
-        Наша компания заключает дологсрочные контракты по уборке различных помещений: офисных, жилых, пригородних и т.д.
-        Штат наших сотрудников - это хорошо обученные клинеры с опытом работы.</h4>
+      <div class="about-us__content">
+        <h2>Мы - профессиональная клининговая компания</h2>
+        <p>За нашими плечами множество коммерчских заказов, а клиенты довольны качеством оказываемых услуг.
+          Мы работаем как с юридическими так и с физическими лицами.
+          Наша компания заключает дологсрочные контракты по уборке различных помещений: офисных, жилых, пригородних и т.д.
+          Штат наших сотрудников - это хорошо обученные клинеры с опытом работы.</p>
+      </div>
     </div>
     <div class="title-of-example"><h1>Примеры наших работ</h1></div>
     <div class="example">
-      <div class="example__first"></div>
-      <div class="example__second"></div>
+      <div class="example__first">
+        <img class="example__first-img" src="/images/was-become2.webp" alt="">
+        <p>До и после "ежедневной" уборки </p>
+      </div>
+      <div class="example__second">
+        <img class="example__second-img" src="/images/was-become1.webp" alt="">
+        <p>До и после уборки "после ремонта"</p>
+      </div>
     </div>
   </div>
 </template>
@@ -37,7 +52,7 @@ export default {
     return {
       slides: [
         { id: '0', isActive: true, img: '1.webp' },
-        { id: '1', isActive: false, img: 'img.jpg' },
+        { id: '1', isActive: false, img: '2.webp' },
         { id: '2', isActive: false, img: 'i.jpg' },
       ],
       currentSlide: 0,
@@ -85,67 +100,132 @@ export default {
 <style lang="scss" scoped>
 
   .home {
-    width: 80%;
+    width: 100%;
     margin: auto;
     padding: 30px 0;
   }
+  .slider-and-main-discriptor {
+    width: 80%;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    gap: 30px;
+    @media screen and (max-width: 750px) {
+      margin: auto;
+      flex-direction: column;
+    }
+  }
+  .main-discriptor {
+    width: 35%;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    hr {
+      width: 100%;
+      height: 1px;
+      background: green;
+    }
+    @media screen and (max-width: 750px) {
+      width: 80%;
+    }
+  }
   .slider {
-    width: 100%;
+    width: 60%;
     height: 350px;
     border: 2px solid green;
     border-radius: 5px;
     display: flex;
     align-items: center;
+    @media screen and (max-width: 750px) {
+      width: 100%;
+      height: 150px;
+    }
 
     &__slds {
       width: 100%;
       height: 100%;
       border-radius: 3px;
+      transition: 1s;
     }
 
     &__sliders-btns {
-      width: 80%;
+      width: 48%;
       height: auto;
       position: absolute;
       display: flex;
       justify-content: space-between;
       align-items: center;
+      @media screen and (max-width: 750px) {
+        width: 80%;
+      }
     }
 
+    &__prev-slide-btn, &__next-slide-btn {
+      height: 50px;
+      width: 50px;
+      position: relative;
+      background: #ccffcc;
+      border: 1px solid green;
+      border-radius: 5px;
+      font-size: 30px;
+      @media screen and (max-width: 750px) {
+        width: 35px;
+        height: 35px;
+        background: rgba(0, 0, 0, 0.5);
+        font-size: 20px;
+      }
+    }
+    &__prev-slide-btn:hover, &__next-slide-btn:hover {
+      background: #74cc74;
+      transition: .5s;
+      @media screen and (max-width: 750px) {
+        background: rgba(0, 0, 0, .9);
+      }
+    }
     &__prev-slide-btn {
-      height: 50px;
-      width: 50px;
-      position: relative;
       left: 20px;
-      border: 1px solid green;
-      border-radius: 5px;
-      font-size: 30px;
     }
-
     &__next-slide-btn {
-      height: 50px;
-      width: 50px;
-      position: relative;
       right: 20px;
-      border: 1px solid green;
-      border-radius: 5px;
-      font-size: 30px;
     }
   }
   .about-us {
-    width: 75%;
+    width: 100%;
     margin: 30px 0;
+    background: #CCFFCC;
+    &__content {
+      width: 80%;
+      margin: auto;
+      padding: 30px 0;
+      p {
+        font-size: 18px;
+      }
+    }
+  }
+  .title-of-example {
+    width: 80%;
+    margin: auto;
   }
   .example {
     height: auto;
-    width: 100%;
+    width: 80%;
+    margin: auto;
+    padding: 30px 0;
     display: flex;
     justify-content: space-between;
-    &__first, &__second {
-      width: 35%;
-      height: 180px;
-      border: 1px solid green;
+    font-size: 20px;
+    @media screen and (max-width: 1350px) {
+      flex-direction: column;
+      gap: 30px;
+    }
+    &__first-img, &__second-img {
+      width: 100%;
+      height: 250px;
+      border: 2px solid green;
       border-radius: 5px;
+      @media screen and (min-width: 800px) {
+        width: 70%;
+      }
     }
   }
 </style>
